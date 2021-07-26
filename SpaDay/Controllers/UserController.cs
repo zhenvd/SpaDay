7771d96cdf969179aa84bb5553c59b27093cf1ae
Vincent Zhen <Vincent.D.Zhen@Outlook.com>
2021-07-26 12:01:17 -0400

    added an if else for verification to send user back to correct form if passwords do not match

--- SpaDay/Controllers/UserController.cs
+++ SpaDay/Controllers/UserController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SpaDay.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +9,36 @@ namespace SpaDay.Controllers
 {
     public class UserController : Controller
     {
+        static private List<User> Users = new List<User>();
         public IActionResult Index()
         {
             return View();
+        }
+        //[HttpGet]
+        //[Route("/user/add/")]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost("/user")]
+        //[Route("/user/add/")]
+        public IActionResult SubmitAddUserForm(User newUser, string verify)
+        {
+            if(newUser.Password == verify)
+            {
+                //User user = new User(newUser.Username, newUser.Email, newUser.Password);
+                ViewBag.users = newUser;
+                return View("Index");
+            }
+            else
+            {
+                ViewBag.users = newUser;
+                ViewBag.error = true;
+                return View("Add");
+            //return View("Add");
+            }
+           
         }
     }
 }
--- SpaDay/Models/User.cs
+++ SpaDay/Models/User.cs
@@ -7,5 +7,19 @@ namespace SpaDay.Models
 {
     public class User
     {
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        //public User()
+        //{
+ 
+        //}
+        //public User(string username, string email, string password) : this()
+        //{
+        //    Username = username;
+        //    Email = email;
+        //    Password = password;
+        //}
     }
 }
